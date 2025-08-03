eccfc5a4e2e8bd5b1438065c112a35b3be3ed972
JianMing <lkienminh@gmail.com>
2025-08-03 19:02:42 +0700

    Create player script

--- Assets/CohenLi/DefenseGameBasic/Scripts/Const.cs
+++ Assets/CohenLi/DefenseGameBasic/Scripts/Const.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Conts
+public class Const
 {
     public const string PLAYER_TAG = "Player";
     public const string ENEMY_TAG = "Enemy";
