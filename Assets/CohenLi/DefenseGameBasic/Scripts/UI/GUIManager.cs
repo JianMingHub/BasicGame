65e057b5fae2ec7491d8b3392067a79460262073
JianMingHub <lkienminh@gmail.com>
2025-08-11 22:01:59 +0700

    Create GUIManager script

---
+++ Assets/CohenLi/DefenseGameBasic/Scripts/UI/GUIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UDEV.DefenseGameBasic
+{
+    public class GUIManager : MonoBehaviour
+    {
+        public GameObject homeGUI;
+        public GameObject gameGUI;
+        public Dialog gameOverDialog;
+        public Text mainCoinTxt;
+        public Text gameplayCoinTxt;
+
+        void Start()
+        {
+
+        }
+
+        public void ShowGameGUI(bool isShow)
+        {
+            if (gameGUI)
+                gameGUI.SetActive(isShow);
+            if (homeGUI)
+                homeGUI.SetActive(!isShow);
+        }
+
+        public void UpdateMainCoins()
+        {
+            if (mainCoinTxt)
+                mainCoinTxt.text = Pref.coins.ToString();
+        }
+
+        public void UpdateGameplayCoins()
+        {
+            if (gameplayCoinTxt)
+                gameplayCoinTxt.text = Pref.coins.ToString();
+        }
+    }
+}
+
---
+++ Assets/CohenLi/DefenseGameBasic/Scripts/UI/SettingDialog.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UDEV.DefenseGameBasic;
+using UnityEngine;
+
+namespace UDEV.DefenseGameBasic
+{
+    public class SettingDialog : Dialog
+    {
+        
+    }
+}
